f001a133fb0b66702c4a12e27d553afab5e46379
juacorreagi <juacorreagi94@gmail.com>
2021-05-25 19:55:21 -0500

    primer commit visitantes

---
+++ express.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const port = 3000;
+
+app.get('/', (request, response) => {
+    mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+    mongoose.connection.on('error', (e) => console.log(e));
+    mongoose.connection.once('open', () => console.log('Mongoose Connected'));
+    const dataSchema = mongoose.Schema({
+        name : String,
+        date : Date,
+    });
+
+    const VisitorModel = mongoose.model('Visitor', dataSchema);
+
+    const firstData = new VisitorModel({
+        name : request.query.name,
+        date : Date.now()
+    });
+
+    firstData.save((error) => {
+        if (error){
+            console.log(error);
+            return
+        }
+        console.log('Document created');
+    
+        response.send("<h1>El visitante fue almacenado con éxito</h1>");
+    });
+        
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
